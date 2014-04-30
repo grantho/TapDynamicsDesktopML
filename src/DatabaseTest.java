66bb593b432f256b83e497cae80d48bbf3ba1ca2
Grant <grantho@stanford.edu>
2014-04-30 11:48:57 -0700

    Basic unit tests for the database

--- src/DatabaseTest.java
+++ src/DatabaseTest.java
@@ -11,8 +11,22 @@ public class DatabaseTest {
 	public void test() {
 		try {
 			Database db = new Database();
-			db.addPerson("bobby", 1111);
-			assert(db.getPin("bobby") == 1111);
+			//db.addPerson("bobby", 1111);
+			//assert(db.getPin("bobby") == 1111);
+			db.closeDatabase();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO Auto-generated catch block
+			fail("Exception thrown");
+		}
+	}
+	
+	@Test
+	public void test2() {
+		try {
+			Database db = new Database();
+			assert(db.getNumUsers() > 10);
+			db.closeDatabase();
 		} catch (Exception e) {
 			e.printStackTrace();
 			// TODO Auto-generated catch block
